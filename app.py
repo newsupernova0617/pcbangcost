242f40b97c9ce5c9a6fbd95513cd27f224010f04
newsupernova0617 <86960667+newsupernova0617@users.noreply.github.com>
2024-05-08 17:03:26 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, jsonify, request
+import sqlite3
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)  # CORS를 전체 앱에 적용
+
+@app.route('/')
+def index():
+    return render_template('map.html')
+
+@app.route('/data')
+def get_data():
+    conn = sqlite3.connect('pc_bangs_info.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, x, y, price FROM pc_bangs")
+    pc_bangs = cursor.fetchall()
+    conn.close()
+    pc_bangs_data = [{'name': row[0], 'x': row[1], 'y': row[2], 'price': row[3]} for row in pc_bangs]
+    return jsonify(pc_bangs_data)
+
+@app.route('/update-price', methods=['POST'])
+def update_price():
+    data = request.json
+    conn = sqlite3.connect('pc_bangs_info.db')
+    cursor = conn.cursor()
+    cursor.execute("UPDATE pc_bangs SET price = ? WHERE name = ?", (data['newPrice'], data['name']))
+    conn.commit()
+    conn.close()
+    return jsonify({'success': True})
+
+if __name__ == '__main__':
+    app.run(debug=True)
