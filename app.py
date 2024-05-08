55c8c7bd966e43b97c96e4ecffbfd9447c938474
newsupernova0617 <86960667+newsupernova0617@users.noreply.github.com>
2024-05-08 22:54:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ def update_price():
     conn.commit()
     conn.close()
     return jsonify({'success': True})
-
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080) 
